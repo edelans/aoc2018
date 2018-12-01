1e62d04f1692e7975e44f82f1048152d7291d7d9
edelans <edelans@gmail.com>
2018-12-02 00:02:16 +0100

    adding some boilerplate for following days

---
+++ TEMPLATE.py
@@ -0,0 +1,39 @@
+# Python 3.x
+from aoc_utilities import Input
+import sys
+import itertools
+
+# day must be 2 digit
+DAY = '01'
+
+
+"""
+PART1
+"""
+
+def solve1(input):
+
+
+
+"""
+PART 2
+"""
+
+
+def solve2(input):
+
+
+
+
+
+if sys.argv[2] == 1:
+    res = solve1((Input(DAY).readlines()))
+    print(res)
+    if sys.argv[3] && sys.argv[3] == 's':
+        submit1(res)
+
+if sys.argv[2] == 2:
+    res = solve1((Input(DAY).readlines()))
+    print(res)
+    if sys.argv[3] && sys.argv[3] == 's':
+        submit2(res)
---
+++ readme.md
@@ -0,0 +1,9 @@
+# why
+
+just having fun sovling [advent of code](https://adventofcode.com/) puzzles one more year =)
+
+# utilities
+
+use this lib ?  https://github.com/wimglenn/advent-of-code-data
+
+    pip install advent-of-code-data
