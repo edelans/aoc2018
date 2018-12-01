ce96843f7073ab86fc7a68906eee0205e5a6fd87
edelans <edelans@gmail.com>
2018-12-01 23:18:27 +0100

    day 1 part 2 OK

--- day01.py
+++ day01.py
@@ -19,3 +19,24 @@ def solve1(input):
 
 # res = solve1((Input(DAY).readlines()))
 # print(res)
+
+"""
+PART 2
+"""
+import itertools
+
+
+def solve2(input):
+    freq = 0
+    freq_set = set([])
+
+    for delta in itertools.cycle(input):
+        freq += int(delta)
+        if freq in freq_set:
+            return freq
+        else:
+            freq_set.add(freq)
+
+
+res = solve2((Input(DAY).readlines()))
+print(res)
