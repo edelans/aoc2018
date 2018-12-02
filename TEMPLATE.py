acfa97c36d2bccf4a398dfdd14608ce60bac781d
edelans <edelans@gmail.com>
2018-12-02 18:13:19 +0100

    fix + linting

--- TEMPLATE.py
+++ TEMPLATE.py
@@ -1,7 +1,9 @@
-# Python 3.x
+#!/usr/bin/env python3
+"""Template file to be used as boilerplate for each day puzzle."""
 from aoc_utilities import Input
+import aocd
 import sys
-import itertools
+# import itertools
 
 # day must be 2 digit
 DAY = '01'
@@ -11,9 +13,10 @@ DAY = '01'
 PART1
 """
 
-def solve1(input):
-    return
 
+def solve1(input):
+    """Solves part 1."""
+    pass
 
 
 """
@@ -22,8 +25,8 @@ PART 2
 
 
 def solve2(input):
-    return
-
+    """Solves part2."""
+    pass
 
 
 if sys.argv[1] == '1':
@@ -31,11 +34,11 @@ if sys.argv[1] == '1':
     print(res)
     if len(sys.argv) == 3:
         if sys.argv[2] == 's':
-            submit1(res)
+            aocd.submit1(res)
 
 if sys.argv[1] == '2':
-    res = solve1((Input(DAY).readlines()))
+    res = solve2((Input(DAY).readlines()))
     print(res)
     if len(sys.argv) == 3:
         if sys.argv[2] == 's':
-            submit2(res)
+            aocd.submit2(res)
