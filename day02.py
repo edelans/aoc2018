4f753e037b1be1f4d2c951f350b065f1f68ab322
edelans <edelans@gmail.com>
2018-12-02 18:57:18 +0100

    part 2 ok

--- day02.py
+++ day02.py
@@ -1,7 +1,8 @@
-# Python 3.x
+#!/usr/bin/env python3
+"""This is a test."""
 from aoc_utilities import Input
+import aocd
 import sys
-import itertools
 
 # day must be 2 digit
 DAY = '02'
@@ -11,7 +12,9 @@ DAY = '02'
 PART1
 """
 
+
 def solve1(input):
+    """Solves part2."""
     count2, count3 = 0, 0
 
     for id in input:
@@ -20,9 +23,9 @@ def solve1(input):
             occurences[letter] = occurences.get(letter, 0) + 1
         values = set(occurences.values())
         if 2 in values:
-            count2+=1
+            count2 += 1
         if 3 in values:
-            count3+=1
+            count3 += 1
 
     return count2*count3
 
@@ -32,20 +35,33 @@ PART 2
 
 
 def solve2(input):
-    return
-
-
+    """Solves part2."""
+    for i in range(0, len(input)):
+        ref = input[i]
+        for j in range(i+1, len(input)):
+            # previous candidates were tested with previous references before
+            candidate = input[j]
+            diff_count, index = 0, 0
+            for k in range(0, len(ref)):
+                if ref[k] != candidate[k]:
+                    diff_count += 1
+                    index = k
+            if diff_count == 1:
+                print("the IDs are :\n{}{}{}".format(ref, candidate, (index)*' ' + '^'))
+                return ref[:index]+ref[index+1:]
 
 if sys.argv[1] == '1':
     res = solve1((Input(DAY).readlines()))
     print(res)
     if len(sys.argv) == 3:
         if sys.argv[2] == 's':
-            submit1(res)
+            print("attempting to submit the response '{}' to part 1: \n\n".format(res))
+            aocd.submit1(res)
 
 if sys.argv[1] == '2':
-    res = solve1((Input(DAY).readlines()))
+    res = solve2((Input(DAY).readlines()))
     print(res)
     if len(sys.argv) == 3:
         if sys.argv[2] == 's':
-            submit2(res)
+            print("attempting to submit the response '{}' to part 2: \n\n".format(res))
+            aocd.submit2(res)
