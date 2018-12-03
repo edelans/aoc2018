9a0790beabeb0f2afb6aa27956ec642908129ca0
edelans <edelans@gmail.com>
2018-12-03 19:22:37 +0100

    part 2 ok

--- day03.py
+++ day03.py
@@ -46,7 +46,44 @@ PART 2
 
 def solve2(input):
     """Solves part2."""
-    pass
+
+    # must parse "#id @ x,y: widthxheight" as in following example :
+    # 1 @ 1,3: 4x4
+    regex = r"^#(?P<id>\d+)\s@\s(?P<x>\d+),(?P<y>\d+):\s(?P<width>\d+)x(?P<height>\d+)"
+
+    # Store the mapping in a dict. Key is coordinates (tuple), value is a list of ID claims.
+    mapping = {}
+
+    # store claim ids
+    id_set = set([])
+
+    for claim in input:
+        matches = re.search(regex, claim)
+        if matches:
+            id = matches.group('id')
+            x, y = int(matches.group('x')), int(matches.group('y'))
+            width, height = int(matches.group('width')), int(matches.group('height'))
+            id_set.add(id)
+            for i in range(x, x + width):
+                for j in range(y, y + height):
+                    mapping[(i, j)] = mapping.get((i, j), []) + [id]
+
+
+    # part 2 really starts here
+
+    overlaping_ids = set([l for k in mapping.values() if len(k) >= 2 for l in k])
+
+    # Tip : remember that nested comprehension list follow the same order of the corresponding for loop
+    # see https://spapas.github.io/2016/04/27/python-nested-list-comprehensions/
+    # for k in mapping.values():
+    #     if len(k) >= 2:
+    #         for l in k:
+    #             l
+
+    for overlap_id in overlaping_ids:
+        id_set.discard(overlap_id)
+
+    return id_set
 
 
 """
